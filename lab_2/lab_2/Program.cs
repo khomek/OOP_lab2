76d96980400759ac07998cf2023b501c2ccce40c
khomek <akir280106@gmail.com>
2025-03-31 20:09:05 +0300

    new update

--- lab_2/lab_2/Program.cs
+++ lab_2/lab_2/Program.cs
@@ -35,33 +35,27 @@ void SyncTask()
     Console.WriteLine("SyncTask");
     var time = new Stopwatch();
     time.Start();
-    Console.WriteLine("URL1 is:");
     var first = asyncTask("https://official-joke-api.appspot.com/random_joke").Result;
-    Console.WriteLine($"{first}\n");
-    Console.WriteLine("URL2 is:");
+    Console.WriteLine($"URL1 is: \n{first}\n");
     var second = asyncTask("https://api.exchangerate-api.com/v4/latest/USD").Result;
-    Console.WriteLine($"{second}\n");
-    Console.WriteLine("URL3 is:");
+    Console.WriteLine($"URL2 is: \n {second}\n");
     var third = asyncTask("https://geek-jokes.sameerkumar.website/api?format=json").Result;
-    Console.WriteLine($"{third}\n");
+    Console.WriteLine($"URL3 is: \n {third}\n");
     time.Stop();
-    Console.WriteLine($"Time for the sync program to work: {time.ElapsedMilliseconds} ms");
+    Console.WriteLine($"Time for the sync program to work: {time.ElapsedMilliseconds} ms\n");
 }
 async Task<string> AsyncTask()
 {
     Console.WriteLine("AyncTask");
     var time = new Stopwatch();
     time.Start();
-    Console.WriteLine("URL1 is:");
     var first1 = await asyncTask("https://official-joke-api.appspot.com/random_joke");
-    Console.WriteLine($"{first1}\n");
-    Console.WriteLine("URL2 is:");
+    Console.WriteLine($"URL1 is: \n{first1}\n");
     var second1 = await asyncTask("https://api.exchangerate-api.com/v4/latest/USD");
-    Console.WriteLine($"{second1}\n");
-    Console.WriteLine("URL3 is:");
+    Console.WriteLine($"URL2 is: \n{second1}\n");
     var third1 = await asyncTask("https://geek-jokes.sameerkumar.website/api?format=json");
-    Console.WriteLine($"{third1}\n");
+    Console.WriteLine($"URL3 is: \n{third1}\n");
     time.Stop();
-    Console.WriteLine($"Time for the async program to work: {time.ElapsedMilliseconds} ms");
+    Console.WriteLine($"Time for the async program to work: {time.ElapsedMilliseconds} ms\n");
     return null;
 }
