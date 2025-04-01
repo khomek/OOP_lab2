e96a0f88239d073ba63c6b99b3b600a1fdc96371
khomek <akir280106@gmail.com>
2025-04-01 18:27:09 +0300

    Update Program.cs

--- lab_2/lab_2/Program.cs
+++ lab_2/lab_2/Program.cs
@@ -36,9 +36,11 @@ void SyncTask(){
     var first = asyncTask("https://official-joke-api.appspot.com/random_joke").Result;
     var second = asyncTask("https://api.exchangerate-api.com/v4/latest/USD").Result;
     var third = asyncTask("https://geek-jokes.sameerkumar.website/api?format=json").Result;
+
     Console.WriteLine($"URL1S is: \n{first}\n");
     Console.WriteLine($"URL2S is: \n {second}\n");
     Console.WriteLine($"URL3S is: \n {third}\n");
+
     time.Stop();
     Console.WriteLine($"Time for the sync program to work: {time.ElapsedMilliseconds} ms\n");
 }
@@ -50,10 +52,13 @@ async Task AsyncTask(){
     var first =   asyncTask("https://official-joke-api.appspot.com/random_joke");
     var second =  asyncTask("https://api.exchangerate-api.com/v4/latest/USD");
     var third =   asyncTask("https://geek-jokes.sameerkumar.website/api?format=json");
+
     await Task.WhenAll(first, second, third);
+
     Console.WriteLine($"URL1 is: \n{first.Result}\n");
     Console.WriteLine($"URL2 is: \n{second.Result}\n");
     Console.WriteLine($"URL3 is: \n{third.Result}\n");
+
     time.Stop();
     Console.WriteLine($"Time for the async program to work: {time.ElapsedMilliseconds} ms\n");
 }
